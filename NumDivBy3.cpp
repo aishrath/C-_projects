d530903b379212c5f8388baee7dace81f2bbe524
aishrath <51309797+aishrath@users.noreply.github.com>
2019-07-11 22:59:51 -0400

    Create NumDivBy3.cpp

---
+++ NumDivBy3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+        for(int i = 0; i <= 10000; i+=1)
+        {
+           if(i%3 == 0)
+           {
+             cout<<i<< endl;
+           }
+        }
+
+
+
+        return 0;
+}
