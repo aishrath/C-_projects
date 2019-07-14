d0e1eb88955e15d980ddb6186d7eeb4d7b5d1286
aishrath <51309797+aishrath@users.noreply.github.com>
2019-07-14 01:39:15 -0400

    Create ForAndWhileLoops.cpp

---
+++ ForAndWhileLoops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+        cout << "Welcome to C++"<< endl;
+        string f = "hope you like it";
+        cout << f << endl;
+
+        for(int i = 0; i < 10; i++)
+        {
+           cout << i << endl;
+        }
+
+        int i = 0;
+        while(i < 5)
+        {
+           i += 1;
+           cout << "you're a funny person" << endl;
+           cout << i << endl;
+        }
+
+        return 0;
+
+}
